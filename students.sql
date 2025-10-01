415b7ae88141e431f2151326dd5453a422746f14
khasebetshepahalo2004-glitch <khasebetshepahalo2004@gmail.com>
2025-10-01 13:54:42 +0200

    Add files via upload

---
+++ students.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Sep 02, 2025 at 08:49 AM
+-- Server version: 9.1.0
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+CREATE TABLE IF NOT EXISTS `students` (
+  `student_id` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(20) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Course` varchar(20) NOT NULL,
+  `Passowrd` varchar(20) NOT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10344125 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`student_id`, `Name`, `Email`, `Course`, `Passowrd`) VALUES
+(10344124, 'Khasebe Tshepahalo', 'Khasebetshepahalo2004@gmail.com', 'IT in Network Manage', 'Passowrd1234');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
